51158448716578bc28f641afdfb363a92b0d3e4a
Nick Counts <nick.counts@gmail.com>
2025-03-05 01:00:51 -0500

    prettier solution to proj4. Also accept lower case

--- ch07/proj4.c
+++ ch07/proj4.c
@@ -3,62 +3,33 @@
 // (-,. etc) preserce it.
 // 2=ABC, 3=DEF, 4=GHI, 5=JKL, 6=MNO, 7=PRS, 8=TUV, 9=WXY
 
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 void print_alpha_to_num(char c) {
   // If the char isn't a capital letter that maps to a phone number/letter
   // key, then it drops through and simply prints the character
-  switch (c) {
-  case 'A':
-  case 'B':
-  case 'C':
+
+  c = toupper(c);
+
+  if ('A' <= c && c <= 'C') {
     printf("2");
-    break;
-
-  case 'D':
-  case 'E':
-  case 'F':
+  } else if ('D' <= c && c <= 'F') {
     printf("3");
-    break;
-
-  case 'G':
-  case 'H':
-  case 'I':
+  } else if ('G' <= c && c <= 'I') {
     printf("4");
-    break;
-
-  case 'J':
-  case 'K':
-  case 'L':
+  } else if ('J' <= c && c <= 'L') {
     printf("5");
-    break;
-
-  case 'M':
-  case 'N':
-  case 'O':
+  } else if ('M' <= c && c <= 'O') {
     printf("6");
-    break;
-
-  case 'P':
-  case 'R':
-  case 'S':
+  } else if ('P' <= c && c <= 'S') {
     printf("7");
-    break;
-
-  case 'T':
-  case 'U':
-  case 'V':
+  } else if ('T' <= c && c <= 'V') {
     printf("8");
-    break;
-
-  case 'W':
-  case 'Y':
-  case 'X':
+  } else if ('W' <= c && c <= 'Y') {
     printf("9");
-    break;
-
-  default:
+  } else {
     printf("%c", c);
   }
 }
