6e48a5338de709e9e7f5764a8933339269ad8f68
Nick Counts <nick.counts@gmail.com>
2025-03-08 15:35:18 -0500

    completed ex6

---
+++ ch08/ex6.c
@@ -0,0 +1,10 @@
+// Build a 2-D array to drive a seven-segment LED display:
+// segments[digit][segment]
+
+const int segments[10][7] = {{1, 1, 1, 1, 1, 1, 0}, {0, 1, 1, 0, 0, 0, 0},
+                             {1, 1, 0, 1, 1, 0, 1}, {1, 1, 1, 1, 0, 0, 1},
+                             {0, 1, 1, 0, 0, 1, 1}, {1, 0, 1, 1, 0, 1, 1},
+                             {1, 0, 1, 1, 1, 1, 1}, {1, 1, 1, 0, 0, 0, 0},
+                             {1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 0, 1, 1}};
+
+int main(int argc, char *argv[]) { return 0; }
