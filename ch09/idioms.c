ad727c91b807f20e0f67dea586f8f11f72c6139b
Nick Counts <nick.counts@gmail.com>
2025-03-11 00:02:48 -0400

    cast to void means discard the return value

--- ch09/idioms.c
+++ ch09/idioms.c
@@ -15,6 +15,9 @@ int main(int argc, char *argv[]) {
   // This is another example of function definition.
   // You can use the alternative main declaration that
   // takes `void`: `int main(void) { return 0;}`
+
+  // Even functions without arguments require empty parentheses when invoked
+  my_void_func();
   return 0;
 }
 
@@ -22,4 +25,11 @@ int main(int argc, char *argv[]) {
 //
 // C99 allows declarations anywhere, as long as they precede the first use
 // c89 required all declaration at the top of a function.
+
+// Casting to void:
 //
+// casting something to void is effectively saying "throw this away"
+// (void) printf("Hi, mom!\n")  ;
+//
+// explicitely discards the return value of printf (which is the number of
+// characters printed). This is implied if no assignment is made.
