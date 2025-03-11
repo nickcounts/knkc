e41594e15c1a531df3558c7e8605e833e8b1da0f
Nick Counts <nick.counts@gmail.com>
2025-03-10 23:57:35 -0400

    Adding to idioms and cleaned Makefile

--- ch09/idioms.c
+++ ch09/idioms.c
@@ -3,4 +3,23 @@
 // "Our programs so far have consisted of just the `main` function"
 // -- oops!
 //
+// void function declarations:
+void my_void_func(void) {
+  // Does something
+}
+
+// C99 will often allow `void my_void_func()` but to be strict, include
+// void when no parameters are defined
+
+int main(int argc, char *argv[]) {
+  // This is another example of function definition.
+  // You can use the alternative main declaration that
+  // takes `void`: `int main(void) { return 0;}`
+  return 0;
+}
+
+// Variable declaration:
+//
+// C99 allows declarations anywhere, as long as they precede the first use
+// c89 required all declaration at the top of a function.
 //
