770a806df4165abd90aa56459d00066f9802c8af
Nick Counts <nick.counts@gmail.com>
2025-03-11 14:55:19 -0400

    functions with array parameters

--- ch09/idioms.c
+++ ch09/idioms.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 // Notes from the text of Chapter 09
 //
 // "Our programs so far have consisted of just the `main` function"
@@ -7,6 +8,11 @@
 void my_void_func(void) {
   // Does something
 }
+
+// Arrays can be "prototyped" or declared before their definition. This allows
+// them to be called in fuctions that are written before the function
+// definition
+void store_zeros(int a[], int n);
 
 // C99 will often allow `void my_void_func()` but to be strict, include
 // void when no parameters are defined
@@ -18,7 +24,26 @@ int main(int argc, char *argv[]) {
 
   // Even functions without arguments require empty parentheses when invoked
   my_void_func();
+
+  int b[50];
+  printf("\nValue of b[] before store_zeros():\n");
+  for (int i = 0; i < 50; i++)
+    printf(" %d", b[i]);
+
+  store_zeros(b, 50);
+
+  printf("\n\nValue of b[] after store_zeros():\n");
+  for (int i = 0; i < 50; i++)
+    printf(" %d", b[i]);
+
+  printf("\n");
   return 0;
+}
+
+void store_zeros(int a[], int n) {
+  int i;
+  for (i = 0; i < n; i++)
+    a[i] = 0;
 }
 
 // Variable declaration:
@@ -33,3 +58,12 @@ int main(int argc, char *argv[]) {
 //
 // explicitely discards the return value of printf (which is the number of
 // characters printed). This is implied if no assignment is made.
+
+// The `exit()` function
+//
+// It's just like `return` in `main()`  - we can use EXIT_FAILURE or
+// EXIT_SUCCESS from `stdlib.h`
+
+// Recursion
+//
+// Can't believe we're packing all of this into chapter 9!
