c90284783af7d1784e307c08ace8ff2e2d312e11
Nick Counts <nick.counts@gmail.com>
2025-09-26 21:20:27 -0400

    Chapter 12 progress

--- ch12/idioms.c
+++ ch12/idioms.c
@@ -57,9 +57,69 @@ void sec_12_1(void) {
   //    0  1  2  3  4  5  6  7  8  9
 }
 
+
+
+void sec_12_2(void) {
+  // Pointers and Arrays
+  printf("\n\n");
+  printf("12.2: Pointers for Array Processing\n----------------------------\n");
+  
+  // int *p = (int []){3, 0, 3, 4, 1};
+
+  // The above is equivalent to:
+  int a[] = {3, 0, 3, 4, 1};
+  int *p = &a[0];
+
+  // the pointer to a compound literal saves the explicit labeling of the
+  // compound literal by assigning it to `a`
+  printf("*p++ or *(p++) Value of exp is *p before increment, inc *p later \n");
+  printf("(*p)++         Value of exp is *p before increment, inc *p later \n");
+  printf("*++p or ++(*p) Inc p first, value of *p is after inc             \n");
+  printf("++*p or ++(*p) Inc p first, value of *p is after inc             \n");
+  
+}
+
+
+void sec_12_3(void) {
+  // Using an Array Name as a Pointer
+  printf("\n\n");
+  printf("12.3: Array Name as a Pointer\n----------------------------------\n");
+  
+  // Given the following declaration:
+  int a[10];
+
+  // We can use the array name as a pointer to the first address:
+  *a = 7; // Stores 7 in a[0]
+  
+  // We can modify a[1] through the pointer a+1
+  *(a+1) = 12; // Stores 12 in a[1]
+
+  printf("Idiom:\n");
+  printf("\nfor (p = a; p < a + N; p++)\n\tsum += *p;\n\n");
+
+  #define N 10
+
+  int sum = 0;
+  int *p;
+
+  for (p = a; p < a + N; p++)
+    sum += *p;
+
+  printf("The array name is ALWAYS a pointer to the first element.\n");
+  printf("You can change the value at its location with *a = x, but\n");
+  printf("You can not change the **value** of a itself (as a pointer)\n");
+  printf("So, a++ is INVALID\n\n");
+
+
+
+}
+
+
+
 int main(int argc, char *argv[]) {
   printf("Chapter 12: Pointers and Arrays\n----------------------------\n\n");
   sec_12_1();
-
+  sec_12_2();
+  sec_12_3();
   return EXIT_SUCCESS;
 }
