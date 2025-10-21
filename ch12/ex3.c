369517697827966116b59ddf7402e349ad8b0d71
Nick Counts <nick.counts@gmail.com>
2025-10-20 20:25:17 -0400

    Trying to add "pretty_print" to handle word wrapping output

--- ch12/ex3.c
+++ ch12/ex3.c
@@ -3,6 +3,51 @@
 
 #define N 10
 
+#define MULTILINE \
+  "This loop reverses the order of an array. The pointers p and q begin pointing " \
+  "to the first and last elements of the array, respectively. The value of the first " \
+  "element is saved to the temp variable. Then the value of the last element is " \
+  "assigned to the first element (through *p++) and then the pointer switces to the " \
+  "second element after the assignment.\n" \
+  "The temp value is assigned to the last element (throught *q--) and then the pointer " \
+  "switches to the penultimate element. This repeats until the pointers reach the " \
+  "middle of the array" 
+
+
+void pretty_print(char* text, int width) {
+  char *start = text;
+  char *search = start;
+  char *last = start;
+  int this_line_width;
+  while (*search != '\0') {
+    this_line_width++;
+
+    if (this_line_width >= width) {
+      // We're too long, drop to last space and print
+      for (char* c = start; c <= last; c++) {
+        printf("%s", c);
+      }
+      printf("\n");
+      // Fix the pointers before continuing
+      last++;
+      start = last;
+      search = start;
+      continue;
+    }
+
+    if (*search == "\n"){
+      // Newline resets line width
+      this_line_width = 0;
+    }
+
+
+    
+  
+  }
+
+}
+
+
 int main() {
   int a[N] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
   int *p = &a[0], *q = &a[N-1], temp;
