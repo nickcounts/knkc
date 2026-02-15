1c987100aacad483ef4cc04c4b270a448d449f0a
Nick Counts <nick.counts@gmail.com>
2026-02-15 11:26:58 -0500

    Chapter 13, Proj 3 complete

--- ch13/proj3.c
+++ ch13/proj3.c
@@ -21,6 +21,40 @@
 
 #define NUM_SUITS 4
 #define NUM_RANKS 13
+#define FULL_NAME_STRLEN 16
+
+typedef struct{
+  char short_name;
+  char full_name[FULL_NAME_STRLEN];
+} card_names ;
+
+card_names CARDS[16] = {
+  {'2', "Two"},
+  {'3', "Three"},
+  {'4', "Four"},
+  {'5', "Five"},
+  {'6', "Six"},
+  {'7', "Seven"},
+  {'8', "Eight"},
+  {'9', "Nine"},
+  {'j', "Jack"},
+  {'q', "Queen"},
+  {'k', "King"},
+  {'a', "Ace"},
+  {'c', "Clubs"},
+  {'d', "Diamonds"},
+  {'h', "Hearts"},
+  {'s', "Spades"},
+};
+
+char * full_string_from_short(char key){
+  for (int i = 0; i < 16; i++){
+    if (CARDS[i].short_name == key){
+      return CARDS[i].full_name;
+    }
+  }
+  return "";
+}
 
 int main(int argc, char *argv[]) {
   bool in_hand[NUM_SUITS][NUM_RANKS] = {false};
@@ -34,14 +68,15 @@ int main(int argc, char *argv[]) {
   printf("Enter the number of cards in hand: ");
   scanf("%d", &num_cards);
 
-  printf("Your hand: ");
+  printf("Your hand: \n");
   while (num_cards > 0) {
     suit = rand() % NUM_SUITS;
     rank = rand() % NUM_RANKS;
     if (!in_hand[suit][rank]) {
       in_hand[suit][rank] = true;
       num_cards--;
-      printf(" %c%c", rank_code[rank], suit_code[suit]);
+      printf("%s of %s\n", full_string_from_short(rank_code[rank]), 
+                           full_string_from_short(suit_code[suit]));
     }
   }
   printf("\n");
