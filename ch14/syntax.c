4eefc880dacbe494db621bb44bd4e882d72ba5eb
Nick Counts <nick.counts@gmail.com>
2026-04-20 11:23:30 -0400

    Chapter 14 - syntax.c updated with 14.2 examples

--- ch14/syntax.c
+++ ch14/syntax.c
@@ -1,3 +1,6 @@
+void print_std_macros();
+
+
 /* Chapter 14, Secion 1
  
    The Preprocessor
@@ -12,7 +15,40 @@
 int main(int argc, char *argv[])
 BEGIN
 
+print_std_macros();
+
 return 0;
 END
 
 
+
+/* Chapter 14, Section 2
+ *
+ * Predefined Macros:
+ *
+ */
+
+#include <stdio.h>
+#define FUNC_CALLED printf("%s called\n", __func__);
+#define FUNC_RETURNED printf("%s returned\n", __func__);
+
+void print_std_macros(){
+  FUNC_CALLED
+
+  printf("%d\n", __LINE__); // int
+  printf("%s\n", __FILE__);
+  printf("%s\n", __DATE__);
+  printf("%s\n", __TIME__);
+  printf("%d\n", __STDC__); // int
+
+  printf("STDC_HOSTED = %d\n", __STDC_HOSTED__);
+  if (__STDC_HOSTED__){
+    printf("C implementation is HOSTED\n");
+  } else {
+    printf("C implementation is STAND-ALONE\n");
+  }
+
+  printf("Supports standard C version %ld\n", __STDC_VERSION__);
+
+  FUNC_RETURNED
+}
