0c0b7ed092064933b94bc1b7795ea3e0db9360c1
Nick Counts <nick.counts@gmail.com>
2026-07-19 18:24:36 -0400

    Chapter 15 - word.c/h

---
+++ ch15/word.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "word.h"
+
+
+int read_char(void){
+  int ch = getchar();
+  return (ch == '\n' || ch == '\t') ? ' ' : ch;
+  // if (ch == '\n' || ch == '\t')
+  //   return ' ';
+  // return ch;
+}
+
+
+// Reads user keyboard input to a buffer.
+// Word is done when character is a space
+// Truncates word if it exceeds length `len`
+// EOF is `^d` on MacOS/Linux and `^z` on Windows
+int read_word(char *word, int len){
+  int ch, pos = 0;
+
+  // Skip any leading spaces
+  while ((ch = read_char()) == ' ')
+    ;
+
+  while (ch != ' ' && ch != EOF) {
+    if (pos < len)
+      word[pos++] = ch;
+    ch = read_char();
+  }
+  word[pos] = '\0';
+  return pos;
+}
+
+
---
+++ ch15/word.h
@@ -0,0 +1,8 @@
+#ifndef WORD_H
+#define WORD_H
+
+#include <stdio.h>
+
+int read_word(char *word, int len);
+
+#endif // define WORD_H
