d7e2b8dbf056d43b498e48d09db1118a5e34332e
Nick Counts <nick.counts@gmail.com>
2026-09-13 12:57:25 -0400

    Chapter 15, Proj 3 complete
    refactored old quicksort into 3 files and updated Makefile

---
+++ ch15/qsort.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "quicksort.h"
+
+#define N 10
+
+
+int main(int argc, char *argv[]) {
+  int a[N], i;
+
+  printf("Enter %d numbers to be sorted: ", N);
+  for (i = 0; i < N; i++)
+    scanf("%d", &a[i]);
+
+  quicksort(a, 0, N - 1);
+  printf("In sorted order: ");
+  for (i = 0; i < N; i++) {
+    printf("%d ", a[i]);
+  }
+  printf("\n");
+
+  return EXIT_SUCCESS;
+}
+
