9261d9844623950cd3ee0acf55454d9356288130
Nick Counts <nick.counts@gmail.com>
2026-09-13 10:04:22 -0400

    Chapter 15, Ex 4 complete

--- ch15/ex04.c
+++ ch15/ex04.c
@@ -53,3 +53,56 @@
  */
 
 
+/*  Answers:
+ *
+ *  a) The output when the program is executed will be:
+ *
+ *  Output if DEBUG is defined:
+ *  Value of i :1
+ *  Value of j :2
+ *  Value of k :3
+ *  Value of i + k :4
+ *  Value of 2 * i + j - k :1
+ *
+ *  I don't think there will be issues with the numerical values but DEBUG.h
+ *  may need some more parenthesis to guard against more complex expressions
+ *
+ *  VERIFIED correct by running the code
+ *
+ *
+ *
+ *
+ *  b) What is the output if the #define directive is removed from `testdebug.c`
+ *
+ *  Output if DEBUG is not defined:
+ *
+ *  VERIFIED correct by running the code
+ *
+ *
+ *
+ * c) Explain why the output is different in parts (a) and (b)
+ *
+ * With the #define directive removed, `debug.h` creates an empty macro 
+ * for PRINT_DEBUG(). Those statements become blank and nothing executes.
+ *
+ * With it defined, PRINT_DEBUG inserts the printf() statements
+ *
+ *
+ *
+ * d) Is it necessary for the DEBUG macro to be defined BEFORE debug.h is 
+ *    included in order for PRINT_DEBUG to have the desired effect? Justify 
+ *    your answer
+ *
+ * I think the preprocessor does a few passes, looking for defines first
+ * and then circling back for the contents of the #include files, so I 
+ * would expect it to work in either order.
+ *
+ *
+ * I appear to have been incorrect! Testing shows that debug.h was pulled
+ * in before the #define and it came in empty. That's a trap for new
+ * players!
+ *
+ */
+
+
+
